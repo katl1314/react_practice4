671492f6a156152de50d4f72eb0185ecade230fc
Choi <nogoso3@gmail.com>
2022-06-12 00:33:42 +0900

    ajax(fetch)이용한 데이터 조회

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import Article from "./components/Article";
+import Nav from "./components/Nav";
+import getJsonList from "./js/ajax";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [title, setTitle] = useState("");
+    const [article, setArticle] = useState("");
+    const [list, setList] = useState([]);
+
+    // render이후 단 한번 호출함. (componentDidMount)
+    useEffect(() => {
+        getJsonList(setList);
+    }, []);
+
+    // setTitle시 title이 변경되면 호출
+    useEffect(() => {
+        document.title = title;
+    }, [title]);
+
+    const setUpdateJsonList = (data, id) => {
+        const { title, description } = data.find((d) => d.id === id);
+        setTitle(title);
+        setArticle(description);
+    };
+
+    return (
+        <div className="App">
+            <header>
+                <h2>WEB</h2>
+            </header>
+            <Nav
+                list={list}
+                onClick={(id) => {
+                    getJsonList((data) => {
+                        setUpdateJsonList(data, id);
+                    });
+                }}
+            />
+            <Article title={title} article={article} />
+        </div>
+    );
 }
 
 export default App;
