047a5bc5c426e688e5f32fcb6b62b50b260b2792
Choi <nogoso3@gmail.com>
2022-06-12 21:49:47 +0900

    클래스형 컴포넌트에서 함수형 컴포넌트로 교체

--- src/App.js
+++ src/App.js
@@ -3,8 +3,84 @@ import React, { useState, useEffect } from "react";
 import Article from "./components/Article";
 import Nav from "./components/Nav";
 import NowLoading from "./components/NowLoading";
-import getJsonList from "./js/ajax";
 
+function App() {
+    var [article, setArticle] = useState({
+        items: {
+            title: "Welcome",
+            description: "Hello, React & Ajax",
+        },
+        isLoading: false,
+    });
+    var [list, setList] = useState({
+        items: [],
+        isLoading: false,
+    });
+
+    // componentDidMount
+    useEffect(() => {
+        var initList = Object.assign({}, list, { isLoading: true });
+        setList(initList);
+        fetch("./data/list.json")
+            .then((res) => res.json())
+            .then((json) => {
+                setTimeout(() => {
+                    setList({
+                        items: json,
+                        isLoading: false,
+                    });
+                }, 1000);
+            });
+        return () => {
+            initList = {};
+        };
+    }, []);
+
+    var NavTag = list.isLoading ? (
+        <NowLoading />
+    ) : (
+        <Nav
+            list={list.items}
+            onClick={(id) => {
+                setArticle(
+                    Object.assign(
+                        {},
+                        { items: article.items },
+                        { isLoading: true }
+                    )
+                );
+                fetch("./data/list.json")
+                    .then((res) => res.json())
+                    .then((json) => {
+                        setTimeout(() => {
+                            var findItem = json.find((d) => d.id === id);
+                            setArticle({
+                                items: findItem,
+                                isLoading: false,
+                            });
+                        }, 1000);
+                    });
+            }}
+        />
+    );
+    var ArticleTag = article.isLoading ? (
+        <NowLoading />
+    ) : (
+        <Article
+            title={article.items.title}
+            description={article.items.description}
+        />
+    );
+
+    return (
+        <div>
+            <h1>WEB</h1>
+            {NavTag}
+            {ArticleTag}
+        </div>
+    );
+}
+/*
 class App extends React.Component {
     state = {
         article: {
@@ -78,5 +154,6 @@ class App extends React.Component {
         );
     }
 }
+*/
 
 export default App;
